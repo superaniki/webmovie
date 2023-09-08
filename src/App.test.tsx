c42fe858604c6142ba1ac6f254067ea67a65bd29
Rickard Sandgren <Rickard.Sandgren@cybercom.com>
2023-09-08 11:02:05 +0200

    First commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// https://github.com/testing-library/react-testing-library
+
+test('First test', () => {
+	render(<App />);
+	const linkElement = screen.getByText(/Funkar/i);
+	expect(linkElement).toBeInTheDocument();
 });
