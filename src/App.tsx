6ecbaf629f62d54c2866ccc8a87456ce77788ec1
Rickard Sandgren <Rickard.Sandgren@cybercom.com>
2023-10-03 09:40:55 +0200

    container ui component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { MovieSearch } from './components/MovieSearch';
+import { Container } from './components/ui';
 import { QueryClient, QueryClientProvider } from 'react-query';
 //import { ReactQueryDevtools } from 'react-query/devtools';
 const queryClient = new QueryClient();
@@ -7,12 +8,9 @@ const queryClient = new QueryClient();
 function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
-			{/*<ReactQueryDevtools initialIsOpen={true} />*/}
-			<div className="absolute w-full min-h-full h-auto bg-slate-200 ">
-				<div className="relative text-center mt-10 ">
-					<MovieSearch />
-				</div>
-			</div>
+			<Container>
+				<MovieSearch />
+			</Container>
 		</QueryClientProvider>
 	);
 }
